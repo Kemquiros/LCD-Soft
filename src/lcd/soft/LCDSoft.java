b1d97eac6567b995d2b1bef4b102ef10d9530396
kemquiros <absurdoeco@gmail.com>
2016-10-28 14:13:16 -0500

    Init

---
+++ src/lcd/soft/LCDSoft.java
@@ -0,0 +1,31 @@
+/*
+    This file is part of LCD-Soft.
+
+    LCD-Soft is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published by
+    the Free Software Foundation.
+
+    LCD-Soft is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with LCD-Soft.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package lcd.soft;
+
+/**
+ *
+ * @author t30r3m4
+ */
+public class LCDSoft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
